c3efdcb539ad7a26227aaa74a3e0ee1936eef474
rnzit <32677893+rnzit@users.noreply.github.com>
2022-10-29 17:02:07 +0700

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,25 @@
+class BubbleSort {
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+
+        }
+    }
+ 
+    public static void main(String args[])
+    {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
